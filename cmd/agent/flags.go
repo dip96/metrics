c20823ec85f1dc45617ddd07c1e2be93963f303d
Dmitry Kosykh <dipp962@gmail.com>
2024-02-11 20:22:21 +0400

    [metrics] - increment 5

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -16,7 +16,7 @@ var flagRuntime int
 func parseFlags() {
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
-	flag.StringVar(&flagRunAddr, "a", "localhost:80", "address and port to run server")
+	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.IntVar(&flagReportInterval, "r", 10, "address and port to run server")
 	flag.IntVar(&flagRuntime, "p", 2, "address and port to run server")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/dip96/metrics
 
-go 1.21.7
+go 1.22rc
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
